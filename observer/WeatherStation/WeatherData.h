f316b5a4bf64de0100e30d3795613230df1be66b
tima55510 <tima55510@gmail.com>
2016-03-23 20:51:46 +0300

    добавлена "жесткая" реализация отображения полной статической информации, в дальнейшем будет улучшено

---
+++ observer/WeatherStation/WeatherData.h
@@ -0,0 +1,147 @@
+﻿#pragma once
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include "Observer.h"
+
+using namespace std;
+
+struct SWeatherInfo
+{
+	double temperature = 0;
+	double humidity = 0;
+	double pressure = 0;
+};
+
+class CDisplay: public IObserver<SWeatherInfo>
+{
+private:
+	/* Метод Update сделан приватным, чтобы ограничить возможность его вызова напрямую
+		Классу CObservable он будет доступен все равно, т.к. в интерфейсе IObserver он
+		остается публичным
+	*/
+	void Update(SWeatherInfo const& data) override
+	{
+		std::cout << "Current Temp " << data.temperature << std::endl;
+		std::cout << "Current Hum " << data.humidity << std::endl;
+		std::cout << "Current Pressure " << data.pressure << std::endl;
+		std::cout << "----------------" << std::endl;
+	}
+};
+
+class CStatsDisplay : public IObserver<SWeatherInfo>
+{
+private:
+	/* Метод Update сделан приватным, чтобы ограничить возможность его вызова напрямую
+	Классу CObservable он будет доступен все равно, т.к. в интерфейсе IObserver он
+	остается публичным
+	*/
+	void Update(SWeatherInfo const& data) override
+	{
+		if (m_minTemperature > data.temperature)
+		{
+			m_minTemperature = data.temperature;
+		}
+		if (m_maxTemperature < data.temperature)
+		{
+			m_maxTemperature = data.temperature;
+		}
+		if (m_minHumidity > data.humidity)
+		{
+			m_minHumidity = data.humidity;
+		}
+		if (m_maxHumidity < data.humidity)
+		{
+			m_maxHumidity = data.humidity;
+		}
+		if (m_minPressure > data.pressure)
+		{
+			m_minPressure = data.pressure;
+		}
+		if (m_maxPressure < data.pressure)
+		{
+			m_maxPressure = data.pressure;
+		}
+		m_accTemperature += data.temperature;
+		m_accHumidity += data.humidity;
+		m_accPressure += data.pressure;
+		++m_countAcc;
+
+		std::cout << "Max Temp " << m_maxTemperature << std::endl;
+		std::cout << "Min Temp " << m_minTemperature << std::endl;
+		std::cout << "Average Temp " << (m_accTemperature / m_countAcc) << std::endl;
+		std::cout << "----------------" << std::endl;
+		std::cout << "Max Humidity " << m_maxHumidity << std::endl;
+		std::cout << "Min Humidity " << m_minHumidity << std::endl;
+		std::cout << "Average Humidity " << (m_accHumidity / m_countAcc) << std::endl;
+		std::cout << "----------------" << std::endl;
+		std::cout << "Max Pressure " << m_maxPressure << std::endl;
+		std::cout << "Min Pressure " << m_minPressure << std::endl;
+		std::cout << "Average Pressure " << (m_accPressure / m_countAcc) << std::endl;
+		std::cout << "----------------" << std::endl;
+	}
+
+	double m_minTemperature = std::numeric_limits<double>::infinity();
+	double m_maxTemperature = -std::numeric_limits<double>::infinity();
+	double m_accTemperature = 0;
+	double m_minHumidity = std::numeric_limits<double>::infinity();
+	double m_maxHumidity = -std::numeric_limits<double>::infinity();
+	double m_accHumidity = 0;
+	double m_minPressure = std::numeric_limits<double>::infinity();
+	double m_maxPressure = -std::numeric_limits<double>::infinity();
+	double m_accPressure = 0;
+	unsigned m_countAcc = 0;
+
+};
+
+class CWeatherData : public CObservable<SWeatherInfo>
+{
+public:
+	// Температура в градусах Цельсия
+	double GetTemperature()const
+	{
+		return m_temperature;
+	}
+	// Относительная влажность (0...100)
+	double GetHumidity()const
+	{
+		return m_humidity;
+	}
+	// Атмосферное давление (в мм.рт.ст)
+	double GetPressure()const
+	{
+		return m_pressure;
+	}
+
+	void MeasurementsChanged()
+	{
+		NotifyObservers();
+	}
+
+	//temp - температура
+	//humidity - влажность
+	//pressure - давление
+
+	void SetMeasurements(double temp, double humidity, double pressure)
+	{
+		m_humidity = humidity;
+		m_temperature = temp;
+		m_pressure = pressure;
+
+		MeasurementsChanged();
+	}
+protected:
+	SWeatherInfo GetChangedData()const override
+	{
+		SWeatherInfo info;
+		info.temperature = GetTemperature();
+		info.humidity = GetHumidity();
+		info.pressure = GetPressure();
+		return info;
+	}
+private:
+	double m_temperature = 0.0;
+	double m_humidity = 0.0;	
+	double m_pressure = 760.0;	
+};
